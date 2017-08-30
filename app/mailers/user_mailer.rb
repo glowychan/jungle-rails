ceb45e5055245410e3e3b186c97b0de26350d9b9
glowychan <glochan.uog@gmail.com>
2017-08-30 13:48:19 -0400

    Create route for mailer and starter code for email template

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def order_email(user, order)
+    @user = user
+    @order = order
+    mail(to: @user.email, subject: 'Thank You for Your Order')
+  end
+end
