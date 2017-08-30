023971fb4a9c1ea44e002e961376213d2d01a871
glowychan <glochan.uog@gmail.com>
2017-08-30 10:15:49 -0400

    Create post route and form to add categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -11,11 +11,19 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-    if @pcategory.save
+    if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
   end
 
 end
